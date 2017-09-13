74de6dbb4b35571e5e81b09221da8e26331dde60
filiprak <filiprak@wp.pl>
2017-09-13 16:10:52 +0200

    Naprawiony skrypt run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 echo "Preparing files"
 SRC=$(find ./src -name "*.java")
-LIBS=$(find ./lib -name "*.jar")
-echo $SRC
+OPENCV=$(find ./lib -name "*.jar" | head -n 1)
+echo "Found opencv lib: $OPENCV"
+echo "Sources: $SRC"
 mkdir -p ./bin
 
 echo "Building application"
-javac -encoding ISO-8859-1 -d ./bin -sourcepath ./src -cp $LIBS $SRC
+javac -encoding ISO-8859-1 -d ./bin -sourcepath ./src -cp $OPENCV $SRC
 
 echo "-------------------------------------------------------------------------------------"
 echo "Running application:"
-java -Djava.library.path=./lib -Dfile.encoding=UTF-8 -classpath ./bin:./lib/opencv-320.jar pl.cpoo.AplikacjaCPOO
+java -Djava.library.path=./lib -Dfile.encoding=UTF-8 -classpath ./bin:$OPENCV pl.cpoo.AplikacjaCPOO
